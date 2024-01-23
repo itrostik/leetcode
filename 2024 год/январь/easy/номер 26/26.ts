caee4872047ed74357eb40f5f9be7d532d80e2ee
itrostik <RosASergeev@stud.kpfu.ru>
2024-01-23 10:19:10 +0300

    меняем архитектуру опять

---
+++ 2024 год/январь/easy/номер 26/26.ts
@@ -0,0 +1,13 @@
+const removeDuplicates = (nums: number[]): number => {
+  let k = 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
+  return k
+};
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
